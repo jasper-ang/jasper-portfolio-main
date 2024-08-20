c8021b5a17aa22a42f991e65394db80a722f4d80
Jasper Ang <jasper.zy.ang@gmail.com>
2024-08-20 05:55:41 +0100

    full crud

--- src/app/components/bloglist.tsx
+++ src/app/components/bloglist.tsx
@@ -1,23 +1,22 @@
-import Link from 'next/link';
-import { Blog } from '../api/blogfetch';
+// import Link from 'next/link';
+// import { Blog } from '../api/blogfetch';
 
-interface BlogListProps {
-  posts: Blog[];
-}
+// interface BlogListProps {
+//   posts: Blog[];
+// }
 
-const BlogList: React.FC<BlogListProps> = ({ posts }) => {
-  return (
-    <section className="bg-base-100 text-base-content">
-      <h2 className="text-3xl font-bold">My Blog</h2>
-      <ul className="bg-base-100 text-base-content space-y-4 rounded py-12">
-        {posts.map(post => (
-          <li key={post._id} className="link-hover border-[#30363d] py-1">
-            <Link href={`/blog/${post._id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-};
+// const BlogList: React.FC<BlogListProps> = ({ posts }) => {
+//   return (
+//     <section className="bg-base-100 text-base-content">
+//       <ul className="bg-base-100 text-base-content space-y-4 rounded py-12">
+//         {posts.map(post => (
+//           <li key={post._id} className="link-hover border-[#30363d] py-1">
+//             <Link href={`/blog/${post._id}`}>{post.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </section>
+//   );
+// };
 
-export default BlogList;
+// export default BlogList;
