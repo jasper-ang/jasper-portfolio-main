040567e7f6c91e3ae85496e138372287076f24f6
Jasper Ang <jasper.zy.ang@gmail.com>
2024-08-23 16:03:29 +0800

    updated UI

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,27 +8,27 @@ export default function BlogPage() {
   // Use SWR to fetch the blog posts
   const { data: posts, error } = useSWR<Blog[]>('/record', fetchBlogPosts);
 
-  if (error) return <div className="text-error mt-8 text-center">Error loading posts.</div>;
-  if (!posts) return <div className="text-info mt-8 text-center">Loading...</div>;
+  if (error) return <div className="mt-8 text-center text-error">Error loading posts.</div>;
+  if (!posts) return <div className="mt-8 text-center text-info">Loading...</div>;
 
   return (
-    <div id="blog" className="bg-base-200 text-base-content p-8">
+    <div id="blog" className="bg-base-200 p-8 text-base-content">
       <div className="mx-auto mb-8 max-w-2xl">
         <h2 className="mb-8 text-left text-4xl font-bold">My Blog</h2>
 
         <div className="mb-6 flex justify-start">
-          <Link href="/blog/newblogpost" className="btn btn-outline btn-ghost">
+          <Link href="/blog/newblogpost" className="btn btn-ghost btn-outline">
             New Page
           </Link>
         </div>
 
-        <div className="bg-base-100 rounded-lg p-6 shadow-lg">
-          <ul className="divide-neutral divide-y">
+        <div className="rounded-lg bg-base-100 p-6 shadow-lg">
+          <ul className="divide-y divide-neutral">
             {posts.map(post => (
               <li key={post._id} className="py-4">
                 <Link
                   href={`/blog/${post._id}`}
-                  className="text-base-content text-sm font-semibold hover:underline"
+                  className="text-sm font-semibold text-base-content transition-colors duration-300 hover:text-primary"
                 >
                   {post.title}
                 </Link>
